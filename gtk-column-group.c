6d61d3c2a72595389b43130b912715db15b9ed8a
Alessandro Grassi <alessandro.g89@gmail.com>
2016-08-26 01:02:38 +0200

    Added sorting function

--- gtk-column-group.c
+++ gtk-column-group.c
@@ -17,6 +17,8 @@
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#include <stdlib.h>
+#include <string.h>
 #include <grp.h>
 #include <libfm/fm.h>
 #include <libfm/fm-gtk.h>
@@ -29,10 +31,25 @@ void get_value(FmFileInfo *fi, GValue *value){
 	g_value_set_string( value, getgrgid( fm_file_info_get_gid( fi ) ) -> gr_name );
 };
 
+gint sort(FmFileInfo *fi1, FmFileInfo *fi2){
+	int result;
+
+	struct group *group1 = getgrgid( fm_file_info_get_gid( fi1 ) );
+	char* gid_string_1 = malloc( strlen( group1 -> gr_name ) + 1 );
+	strcpy(gid_string_1, group1 -> gr_name);
+
+	struct group *group2 = getgrgid( fm_file_info_get_gid( fi2 ) );
+	char* gid_string_2 = group2 -> gr_name;
+
+	result = strcmp( gid_string_1, gid_string_2 );
+	free( gid_string_1 );
+	return result;
+}
+
 FmFolderModelColumnInit fm_module_init_gtk_folder_col = {
 	"Group",
 	0, 
 	&get_type,
 	&get_value,
-	NULL
+	&sort
 };
